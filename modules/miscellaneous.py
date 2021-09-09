bd564a1808ab68c154fe2980ba9a40692a34d0c2
Ahxius <52114171+Ahxius@users.noreply.github.com>
2021-09-08 20:26:42 -0500

    newtrial command

--- modules/miscellaneous.py
+++ modules/miscellaneous.py
@@ -14,6 +14,19 @@ class Miscellaneous(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.command(name='newtrial', aliases=['nt', 'trial'], hidden=True)
+    async def newtrial(self, context):
+        member_roles = context.author.roles
+        phantom_server = self.client.get_guild(364962599508508672)
+        shroud_object = phantom_server.get_role(761957228151832587)
+        to_object = phantom_server.get_role(607971962873970690)
+        if shroud_object not in member_roles and to_object not in member_roles:
+            await context.send("This command requires either the ``SHROUD`` or ``Trial Overseer`` role in "
+                               "order to use.")
+            return
+        await asyncio.sleep(604800)
+        await context.send(f"{to_object.mention} - <@{context.message.raw_mentions[0]}>'s trial has ended.")
+
     @commands.command(name='send', aliases=['s'], help='Sends message as PHANTOM bot')
     async def send(self, context, *, content: str = 0):
         member_roles = context.author.roles
