8575897ea9dad4dc883cd305db8bef366253806e
Ahxius <52114171+Ahxius@users.noreply.github.com>
2022-01-03 16:02:25 -0600

    remove command (maybe works)

--- modules/ProbationaryPortal.py
+++ modules/ProbationaryPortal.py
@@ -343,7 +343,7 @@ class ProbationaryPortal(commands.Cog):
             await context.send(f"{context.author.mention}, make sure you have the VEIL role and are providing a user.")
             return
         try:
-            c.execute(f"DELETE FROM probieInfo WHERE username='{user}'")
+            c.execute(f"DELETE FROM probieInfo WHERE username = '{user}'")
             await context.send(f"``{user}`` successfully removed from database.")
             await self.client.get_server(364962599508508672).get_channel(676604257905934399)\
                 .send(f'{user} removed from database by {context.author.mention}.')
